cebc557f9ef844323756e2a395c8edf458207a4d
anna brezhneva <brezhneva.a@yandex.ru>
2022-12-13 00:07:39 +0300

    Решение задачи №50

---
+++ 50_zadacha/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+using System;
+using static System.Console;
+Clear();
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, -10, 10);
+PrintArray(array);
+
+WriteLine();
+ZnachenieFindElement(array, 1, 7);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void ZnachenieFindElement(int[,] array, int i, int j)
+{
+    int find = 0;
+    if (i < array.GetLength(0) && j < array.GetLength(1))
+    {
+       WriteLine($"Значение элемента на позиции [{i},{j}] равно {array[i,j]}");
+    }
+    else 
+    {
+       WriteLine("Такого элемента нет");
+    }
+}
